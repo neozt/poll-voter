913c3746b7ccd97593612cfbb210f6c51650fb16
Neo Zhen Ting <neozt99@gmail.com>
2026-04-16 18:25:16 +0800

    refactor(db): optimize poll_overview using lateral join

--- schema.sql
+++ schema.sql
@@ -27,17 +27,24 @@ CREATE TABLE vote
     voted_at           TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE INDEX idx_vote_option_id ON vote(selected_option_id);
+
 create or replace view poll_overview as
 select poll.poll_id,
-       poll.title                                                                   as poll_title,
-       poll.description                                                             as poll_description,
+       poll.title                         as poll_title,
+       poll.description                   as poll_description,
        poll.created_at,
        poll.created_by,
        poll.is_active,
        option.option_id,
-       option.title                                                                 as option_title,
-       option.description                                                           as option_description,
-       (select count(*) from vote where vote.selected_option_id = option.option_id) as vote_count
+       option.title                       as option_title,
+       option.description                 as option_description,
+       coalesce(vote_tally.vote_count, 0) as vote_count
 from poll
-         left join option on poll.poll_id = option.poll_id
+left join option on poll.poll_id = option.poll_id
+LEFT JOIN LATERAL (
+    SELECT COUNT(*) AS vote_count
+    FROM vote
+    WHERE vote.selected_option_id = option.option_id
+) vote_tally ON TRUE
 ;
